61ab5207bf7cac09fbe1b6e64af8c18cf343e070
Vadim Hmys <vadimhmyz@gmail.com>
2023-05-05 22:45:39 +0300

    feat: implement func copyDir in the task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,50 @@
+const fs = require('fs/promises');
+const { readdir } = fs;
+const path = require('path');
+ 
+async function copyDir() {
+  try {
+    fs.mkdir(
+      path.join(__dirname, 'files-copy'),
+      { recursive: true },
+      error => {
+        if (error) console.log(error);
+      }
+    );
 
+    const filesFromDirFiles = await readdir(
+      path.join(__dirname, 'files')
+    );
+
+    const filesFromDirFilesCopy = await readdir(
+      path.join(__dirname, 'files-copy')
+    );
+
+    let filesToBeDeleted = [];
+
+    if (filesFromDirFilesCopy.length !== 0) {
+      filesToBeDeleted = filesFromDirFilesCopy.filter( file => !filesFromDirFiles.includes(file));
+
+      for (const file of filesToBeDeleted) {
+        fs.unlink(
+          path.join(__dirname, 'files-copy', `${file}`),
+          error => {
+            if (error) console.log('Error', error.message);
+          }
+        );
+      }
+    }
+    
+    for (const file of filesFromDirFiles) {
+      fs.copyFile(
+        path.join(__dirname, 'files', `${file}`),
+        path.join(__dirname, 'files-copy', `${file}`)
+      );
+    }
+
+  } catch (error) {
+    console.log('Error', error.message);
+  } 
+}
+
+copyDir();
