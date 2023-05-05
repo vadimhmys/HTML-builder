a185d7cb9cd414a0f7c1ecfc2fc8e0ab8b108f3b
Vadim Hmys <vadimhmyz@gmail.com>
2023-05-05 19:59:46 +0300

    feat: perform task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(
+  path.join(__dirname, 'secret-folder'),
+  {withFileTypes: true},
+  (error, items) => {
+    if (error) console.log('Error', error.message);
+    
+    const arr = items.filter( item => !item.isDirectory() );
+
+    for (let i = 0; i < arr.length; i++) {
+      let infoAboutFile = '';
+      const posPoint = arr[i].name.indexOf('.');
+      const fileName = arr[i].name.slice(0, posPoint);
+      const extFile = path.extname(
+        path.join(__dirname, 'secret-folder', `${arr[i].name}`)
+      ).slice(1);
+      fs.stat(
+        path.join(__dirname, 'secret-folder', `${arr[i].name}`),
+        (error, stats) => {
+          if (error) console.log('Error', error.message);
+          infoAboutFile = fileName + ' - ' + extFile + ' - ' + stats['size'];
+          console.log(infoAboutFile);
+        }
+      );
+    }
+
+    //console.log(arr);
+  });
