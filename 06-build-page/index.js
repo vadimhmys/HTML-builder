d6cf9860721b2afee4a5fa3f338e4d88ceeabccc
Vadim Hmys <vadimhmyz@gmail.com>
2023-05-06 19:44:21 +0300

    feat: create function copyDir for copying directories and copy directory assets

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -11,8 +11,39 @@ function createDir(path) {
   );
 }
 
+function copyDir(fromHere, there) {
+  fs.readdir(
+    fromHere,
+    {withFileTypes: true},
+    (error, dirents) => {
+      if(error) console.log('Big error', error.message);
+      for (const dirent of dirents) {
+        if (dirent.isDirectory()) {
+          createDir(path.join(`${there}`, `${dirent.name}`));
+          copyDir(path.join(`${fromHere}`, `${dirent.name}`), path.join(`${there}`, `${dirent.name}`));
+        } else {
+          fs.copyFile(
+            path.join(`${fromHere}`, `${dirent.name}`),
+            path.join(`${there}`, `${dirent.name}`),
+            (err) => {
+              if (err) console.log('Error Found:', err);
+              console.log(there);
+            }
+          );
+        }
+      }
+    
+    });
+}
+
 function main() {
   createDir(path.join(__dirname, 'project-dist'));
+  createDir(path.join(__dirname, 'project-dist', 'assets'));
+  copyDir(
+    path.join(__dirname, 'assets'),
+    path.join(__dirname, 'project-dist', 'assets')
+  );
+  
   
 }
 
