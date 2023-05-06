6c89c5d3c86248751307d70c173f281ad4a2cb44
Vadim Hmys <vadimhmyz@gmail.com>
2023-05-06 22:57:34 +0300

    feat: implement merging all css-files in one file

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { readdir } = fs.promises;
 
 function createDir(path) {
   fs.mkdir(
@@ -44,6 +45,34 @@ function createFile(path) {
   );
 }
 
+async function mergeStyleFiles() {
+  let files = await readdir(
+    path.join(__dirname, 'styles'),
+    {withFileTypes: true}
+  );
+
+  files = files
+    .filter(dirent => dirent.name.endsWith('.css') && !dirent.isDirectory())
+    .map(dirent => dirent.name);
+
+  for (const file of files) {
+    fs.readFile(
+      path.join(__dirname, 'styles', `${file}`),
+      'utf-8',
+      (error, data) => {
+        if (error) console.log('Error', error.message);
+        fs.appendFile(
+          path.join(__dirname, 'project-dist', 'style.css'),
+          data,
+          error => {
+            if (error) console.log('Error', error.message);
+          }
+        );
+      }
+    );
+  }
+}
+
 function main() {
   createDir(path.join(__dirname, 'project-dist'));
   createDir(path.join(__dirname, 'project-dist', 'assets'));
@@ -52,8 +81,8 @@ function main() {
     path.join(__dirname, 'project-dist', 'assets')
   );
   createFile(path.join(__dirname, 'project-dist', 'index.html'));
-  
-  
+  createFile(path.join(__dirname, 'project-dist', 'style.css'));
+  mergeStyleFiles();
 }
 
 main();
